f2447440fc6d7a95fa38253dc78494631291c0b5
shaolonger <1057357150@qq.com>
2018-10-07 21:47:41 +0800

    [new]新增轮播图标，完成轮播组件

--- app/containers/Home/index.jsx
+++ app/containers/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Category from '../../components/Category';
+
 export default class Home extends React.Component{
     render() {
         return (
             <div>
-                Home页面
+                <Category />
             </div>
         );
     }
