cb68dae6cb134f8cae99d5c7f060e3622f648cb9
shaolonger <1057357150@qq.com>
2018-10-08 19:45:10 +0800

    [new]完成city页header

--- app/containers/City/index.jsx
+++ app/containers/City/index.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 
 // components
+import CityHeader from '../../components/CityHeader';
 import CurrentCity from '../../components/CurrentCity';
 
-export default class City extends React.Component{
+// redux
+import { connect } from 'react-redux';
+
+class City extends React.Component{
     render() {
+        const cityName = this.props.cityName;
         return (
             <div>
-                <CurrentCity cityName={'北京'} />
+                <CityHeader title="选择城市" />
+                <CurrentCity cityName={cityName} />
             </div>
         );
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        cityName: state.cityInfo
+    }
+}
+
+export default connect(mapStateToProps)(City)
