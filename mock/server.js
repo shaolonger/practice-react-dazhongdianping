f0dd75cbfdf07fe808870c1001bffe0bb516a0c7
shaolonger <1057357150@qq.com>
2018-10-08 12:05:40 +0800

    [update]更新mock/server.js，增加'超值特惠'部分模拟后台mock获取json数据

--- mock/server.js
+++ mock/server.js
@@ -4,14 +4,20 @@ const koaBody = require('koa-body')();
 
 var app = new KOA();
 
-router.get('/api/get', async ctx => {
-    ctx.body = 'hellow koa';
-});
-router.post('/api/post', koaBody, ctx => {
-    ctx.body = {
-        a: 1,
-        b: false
-    }
+var HomeAD = require('./Home/homead');
+
+// router.get('/api/get', async ctx => {
+//     ctx.body = 'hellow koa';
+// });
+// router.post('/api/post', koaBody, ctx => {
+//     ctx.body = {
+//         a: 1,
+//         b: false
+//     }
+// });
+
+router.get('/api/homead', ctx => {
+    ctx.body = HomeAD;
 });
 
 app.use(router.routes());
