f3ee47d9ca1a3dbd4a3efb2a9733c668b4aadc11
shaolonger <1057357150@qq.com>
2018-10-09 15:17:05 +0800

    [new]完成Detail页Header.jsx

---
+++ app/components/Header/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './style.less';
+
+export default class Header extends React.Component{
+    render() {
+        return (
+            <div id="common-header">
+                <span className="back-icon" onClick={this.clickHandle.bind(this)}>
+                    <i className="icon-chevron-left"></i>
+                </span>
+                <h1>{this.props.title}</h1>
+            </div>
+        );
+    }
+    clickHandle() {
+        window.history.back()
+    }
+}
--- app/containers/Detail/index.jsx
+++ app/containers/Detail/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+// components
+import Header from '../../components/Header';
+
 export default class extends React.Component{
     render() {
         return (
-            <div>detail页面</div>
+            <div>
+                <Header title="商户详情" />
+            </div>
         );
     }
 }
