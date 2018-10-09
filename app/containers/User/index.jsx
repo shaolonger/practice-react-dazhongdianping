81d14d2da831208603c8c79ab52d91716b14fac2
shaolonger <1057357150@qq.com>
2018-10-09 20:27:21 +0800

    [new]完成User用户页面头部信息

--- app/containers/User/index.jsx
+++ app/containers/User/index.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
-export default class User extends React.Component{
+// redux
+import { connect } from 'react-redux';
+
+// components
+import Header from '../../components/Header';
+import UserInfo from '../../components/UserInfo';
+
+class User extends React.Component{
     render() {
         return (
-            <div>User页面</div>
+            <div>
+                <Header title="用户主页" />
+                <UserInfo username={this.props.userInfo.username} city={this.props.cityName} />
+            </div>
         );
     }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        userInfo: state.userInfo,
+        cityName: state.cityInfo
+    }
+}
+
+export default connect(mapStateToProps)(User)
