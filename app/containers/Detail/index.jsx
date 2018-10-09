a629b0b623ff087ba92e78e6df35a069a5fcf076
shaolonger <1057357150@qq.com>
2018-10-09 15:10:31 +0800

    [update]HomeList新增Link跳转到Detail详情页

--- app/containers/Detail/index.jsx
+++ app/containers/Detail/index.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 
 export default class extends React.Component{
-    componentDidMount() {
-        console.log('id', this.props.match.params.id);
-    }
     render() {
         return (
             <div>detail页面</div>
